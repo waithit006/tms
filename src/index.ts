9cd2df8386b8a99aa5dcd3ce7eae5529a07906a6
Waithid Puttasomsri <waithit006@gmail.com>
2021-03-14 22:22:42 +0700

    add typeorm

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,27 @@ import express, { Application, Request, Response } from 'express'
 import { createServer } from 'http'
 import { Server, Socket } from 'socket.io'
 import "reflect-metadata";
+import { createConnection } from "typeorm";
 
 
 const app: Application = express()
 
 const port: number = 3001 || process.env.PORT
 
+createConnection().then(async connection => {
+    const httpServer = createServer(app);
 
-const httpServer = createServer(app);
+    const io = new Server(httpServer, {
 
-const io = new Server(httpServer, {
+    })
 
-})
+    httpServer.listen(port, function () {
+        console.log(`App is listening on port ${port} !`);
 
-httpServer.listen(port, function () {
-    console.log(`App is listening on port ${port} !`);
+    })
 
-})
+}).catch(error => console.log(error));
+
+
+
 
