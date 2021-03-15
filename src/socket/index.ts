d25c6e0f1205ee223c97b0b883e8cb669cd333da
Waithid <waithit006@gmail.com>
2021-03-16 00:11:12 +0700

    socket UPdateLat

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,8 +1,13 @@
 import { Server, Socket } from 'socket.io'
-
+import NodeCache from 'node-cache'
 
 export default (io: Server) => {
     io.on("connection", (socket: Socket) => {
         console.log("Connection");
+
+        socket.on('updateLatLng', (message: any) => {
+            console.log(message);
+        })
+
     })
 }
